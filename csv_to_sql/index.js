d26b7d357fe16bd521d8dc3e8f4d7d897c2eb70b
exson6969 <amyexson@gmail.com>
2023-06-14 00:26:31 +0530

    update csvToSql

--- csv_to_sql/index.js
+++ csv_to_sql/index.js
@@ -1,4 +1,5 @@
 const startCSVToSQL = require('./utils/csvToSql');
 
+const data_file_path = "../schema/rating.json";
 // Start the CSV to SQL conversion
-startCSVToSQL();
+startCSVToSQL(data_file_path);
--- csv_to_sql/utils/csvToSql.js
+++ csv_to_sql/utils/csvToSql.js
@@ -1,37 +1,62 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 const db = require('../config/dbConfig');
+require("dotenv").config()
 
-function startCSVToSQL() {
-  const schema = require('../schema/movie.json');
-  const dbName = schema.databaseName
-  const tableName = schema.tableName
-  const filePath = schema.csvFilePath
-  console.log(filePath)
+function startCSVToSQL(data_file_path) {
+  const schema = require(data_file_path);
+  const dbName = process.env.DB_DATABASE;
+  const tableName = schema.tableName;
+  const filePath = schema.csvFilePath;
 
-  fs.createReadStream(filePath)
-    .pipe(csv())
-    .on('data', (row) => {
-      const data = {};
+  db.query(`CREATE DATABASE IF NOT EXISTS ${dbName}`, (err) => {
+    if (err) {
+      console.error('Error creating database:', err);
+      return;
+    }
 
-      Object.keys(row).forEach((key) => {
-        const column = schema.columns.find((col) => col.name === key);
-        if (column) {
-          data[key] = column.datatype === 'INT' ? parseInt(row[key]) : row[key];
-        }
-      });
+    db.query(`USE ${dbName}`, (err) => {
+      if (err) {
+        console.error('Error selecting database:', err);
+        return;
+      }
 
-      const query = DataQuery(tableName, schema.columns, data);
+      const columnDefinitions = schema.columns
+        .map((column) => `${column.name} ${column.datatype}`)
+        .join(', ');
 
-      db.query(query, Object.values(data), (err) => {
+      db.query(`CREATE TABLE IF NOT EXISTS ${tableName} (${columnDefinitions})`, (err) => {
         if (err) {
-          console.error('Error inserting data:', err);
+          console.error('Error creating table:', err);
+          return;
         }
+
+        fs.createReadStream(filePath)
+          .pipe(csv())
+          .on('data', (row) => {
+            const data = {};
+
+            Object.keys(row).forEach((key) => {
+              const column = schema.columns.find((col) => col.name === key);
+              if (column) {
+                data[key] = column.datatype === 'INT' ? parseInt(row[key]) : row[key];
+              }
+            });
+
+            const query = DataQuery(tableName, schema.columns, data);
+
+            db.query(query, Object.values(data), (err) => {
+              if (err) {
+                console.error('Error inserting data:', err);
+              }
+            });
+          })
+          .on('end', () => {
+            console.log('CSV file successfully imported to the database.');
+          });
       });
-    })
-    .on('end', () => {
-      console.log('CSV file successfully imported to the database.');
     });
+  });
 }
 
 function DataQuery(tableName, columns, row) {
@@ -39,7 +64,7 @@ function DataQuery(tableName, columns, row) {
   const placeholders = columns.map(() => '?').join(', ');
   const values = columns.map((column) => {
     const value = row[column.name];
-    return column.datatype === 'INT' ? parseInt(value) : value;
+    return value;
   });
 
   return `INSERT INTO ${tableName} (${columnNames}) VALUES (${placeholders})`;
