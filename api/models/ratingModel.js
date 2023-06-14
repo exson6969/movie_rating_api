fa19636721d48838db4c5e836219e868bd90f072
exson6969 <amyexson@gmail.com>
2023-06-14 20:54:02 +0530

    getTopRatedMovies error fix

--- api/models/ratingModel.js
+++ api/models/ratingModel.js
@@ -4,8 +4,8 @@ class Rating {
   static getTopRatedMovies(callback) {
     const query = `
       SELECT m.tconst, m.primaryTitle, m.genres, AVG(r.averageRating) AS averageRating
-      FROM movies AS m
-      JOIN ratings AS r ON m.tconst = r.tconst
+      FROM movie AS m
+      JOIN rating AS r ON m.tconst = r.tconst
       WHERE r.averageRating > 6.0
       GROUP BY m.tconst, m.primaryTitle, m.genres
       ORDER BY averageRating DESC
